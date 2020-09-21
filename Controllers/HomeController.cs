fbfc6834b6af79355c3ae0c99e1e3490b2367ac3
Jeffrey Hirai <jhirai20@gmail.com>
2020-09-20 20:57:31 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomPasscode.Models;
+
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("Password", new PasswordGen().GetPassword(14));
+            HttpContext.Session.SetInt32("Count", PasswordGen.GetCount());
+            ViewBag.Password =  HttpContext.Session.GetString("Password");
+            ViewBag.Count = HttpContext.Session.GetInt32("Count");
+            return View();
+        }
+        [HttpGet("/password")]
+        public JsonResult Password()
+        {
+            return Json(new PasswordGen().GetPassword(14));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PasswordGen.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace RandomPasscode.Models
+{
+    public class PasswordGen
+    {
+        private const string _chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private Random rand = new Random();
+        private static int _count = 0;
+
+        public PasswordGen()
+        {
+            _count += 1;
+        }
+
+        public string GetPassword(int length)
+        {
+            var charArray = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                charArray[i] = _chars[rand.Next(_chars.Length)];
+            }
+
+            return string.Join("", charArray);
+            
+        }
+
+        public static int GetCount()
+        {
+            return _count;
+        }
+    }
+}
